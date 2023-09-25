c25dc6d5e47378acf5640539dc9f177d135ed130
Scripter2022 <stress7438@gmail.com>
2023-09-25 21:08:33 +0300

    Возврат к версии без rl модуля

--- scr/lib/dialog.js
+++ scr/lib/dialog.js
@@ -1,16 +1,16 @@
 const readline=require('readline');
 
-exports.getDialog=()=>{
-    
 var rl=readline.createInterface({
-    input:process.stdin,
-    output:process.stdout
+  input:process.stdin,
+  output:process.stdout
 })
 
-rl.question("What your host IP?",function(answer){
-    //return(answer);
-  return (answer)
-});
-
+exports.getDialog=()=>{
+    
+  rl.question("What is your host? ", function(answer) {
+    console.log("Your host is:", answer);
+    return answer
+    rl.close();
+  });
 }
 
